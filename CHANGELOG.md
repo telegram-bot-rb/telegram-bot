17260cb5a9e147805b10293bb90b3c07fecc25fb
Max Melentiev <melentievm@gmail.com>
2017-02-10 09:34:15 +0300

    v0.11.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# Unreleased
+# 0.11.0
 
 - Remove Bot::StaleChat in favor of Bot::Forbidden, as Telegram adds more
   and more new descriptions.
--- lib/telegram/bot/version.rb
+++ lib/telegram/bot/version.rb
@@ -1,6 +1,6 @@
 module Telegram
   module Bot
-    VERSION = '0.10.2'.freeze
+    VERSION = '0.11.0'.freeze
 
     def self.gem_version
       Gem::Version.new VERSION
