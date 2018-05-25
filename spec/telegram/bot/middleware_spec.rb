050df20533778130b50d9011e8b53542cd0e1424
Max Melentiev <melentievm@gmail.com>
2018-05-25 16:35:23 +0300

    Require action_dispatch fully
    It uses autoload, so it's lazy-loaded. However there are missing some
    internal requires so it will asks for more not-related `requires`
    when requiring some files directly.

--- spec/telegram/bot/middleware_spec.rb
+++ spec/telegram/bot/middleware_spec.rb
@@ -20,7 +20,6 @@ RSpec.describe Telegram::Bot::Middleware do
     if ActionPack::VERSION::MAJOR < 5
       # Before Rails 5, params are parsed in middleware.
       # In Rails 5, they are parsed in Request#request_parameters.
-      require 'action_dispatch/middleware/params_parser'
       let(:instance) { ActionDispatch::ParamsParser.new(super()) }
     end
 
